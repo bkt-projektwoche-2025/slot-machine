c1e90208276d8e18a1000a909770fce568c1f41a
waxel30897 <161823221+waxel30897@users.noreply.github.com>
2025-02-06 14:51:48 +0100

    Erweiterung der Datenbank
    Wie heute in der Schule besprochen, hier noch die neue SQL-Datei mit alle Tabellen.

--- slot-maschine.sql
+++ slot-maschine.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 04. Feb 2025 um 16:13
+-- Erstellungszeit: 06. Feb 2025 um 12:24
 -- Server-Version: 10.4.32-MariaDB
--- PHP-Version: 8.0.30
+-- PHP-Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,8 +18,34 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Datenbank: `ita22_dat_slot-maschine_wehn`
+-- Datenbank: `slot-maschine`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `game`
+--
+
+CREATE TABLE `game` (
+  `ID` int(11) NOT NULL,
+  `user_ID` int(11) NOT NULL,
+  `coins` int(11) NOT NULL,
+  `slot_1` int(11) NOT NULL,
+  `slot _2` int(11) NOT NULL,
+  `slot _3` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `session`
+--
+
+CREATE TABLE `session` (
+  `token` varchar(55) NOT NULL,
+  `user_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -37,8 +63,21 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Daten für Tabelle `user`
+--
+
+INSERT INTO `user` (`ID`, `Name`, `Vorname`, `Mail`, `Passwort`, `Geburtsdatum`) VALUES
+(1, 'Wehn', 'Axel', 'wehnaxel@gmail.com', '$2a$11$zZn/DKaOLh0EpEU4TYYozOnIXE2WkIUzUk9mw3sY4yCCl7BZt9Dbe', '1997-08-30');
+
+--
 -- Indizes der exportierten Tabellen
 --
+
+--
+-- Indizes für die Tabelle `game`
+--
+ALTER TABLE `game`
+  ADD PRIMARY KEY (`ID`);
 
 --
 -- Indizes für die Tabelle `user`
@@ -54,7 +93,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT für Tabelle `user`
 --
 ALTER TABLE `user`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
